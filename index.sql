e04bf1026e99824d27ca60eaccbf75dcfe654649
Mohammed8055 <afnan.mohammed8055@gmail.com>
2026-01-06 23:33:03 +0530

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,17 @@
+# Database Credentials
+DB_CONFIG = {
+    "mysql": {
+        "user": "admin_user",
+        "pass": "SecurePass123!",
+        "host": "192.168.1.50",
+        "port": 3306,
+        "db": "sales_records"
+    },
+    "postgres": {
+        "user": "pg_admin",
+        "pass": "PostgresPass456!",
+        "host": "localhost",
+        "port": 5432,
+        "db": "analytics_dw"
+    }
+}
